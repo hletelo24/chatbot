b82701a5c98ef0c7bf1088ab36f48fb7e2a571be
hletelo24 <st10479122@rcconnect.edu.za>
2026-06-13 09:37:20 +0200

    Final game implementation

---
+++ chatbot/CyberTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace chatbot
+{
+    class CyberTask
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? ReminderDate { get; set; }
+        public bool Completed { get; set; }
+    }
+}
---
+++ chatbot/QuizQuestion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace chatbot
+{
+    class QuizQuestion
+    {
+        public string Question { get; set; }
+        public string[] Options { get; set; }
+        public string CorrectAnswer { get; set; }
+        public string Explanation { get; set; }
+    }
+}
